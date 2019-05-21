1704d7708f09194cae48c51963128cb424d9742c
percy mugadza <percym@localhost.localdomain>
2019-05-21 22:36:53 +0200

    fixed logout

--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -1,24 +1,44 @@
-import 'package:first_app/pages/product_edit.dart';
-import 'package:first_app/scoped-models/main.dart';
 import 'package:flutter/material.dart';
+
 import 'package:scoped_model/scoped_model.dart';
+
+import './product_edit.dart';
+import '../scoped-models/main.dart';
 
 class ProductListPage extends StatefulWidget {
   final MainModel model;
 
   ProductListPage(this.model);
+
   @override
   State<StatefulWidget> createState() {
     return _ProductListPageState();
   }
 }
-class _ProductListPageState extends State<ProductListPage>{
 
+class _ProductListPageState extends State<ProductListPage> {
   @override
-  void initState() {
+  initState() {
     widget.model.fetchProducts();
     super.initState();
   }
+
+  Widget _buildEditButton(BuildContext context, int index, MainModel model) {
+    return IconButton(
+      icon: Icon(Icons.edit),
+      onPressed: () {
+        model.selectProduct(model.allProducts[index].id);
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (BuildContext context) {
+              return ProductEditPage();
+            },
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return ScopedModelDescendant<MainModel>(
@@ -29,25 +49,26 @@ class _ProductListPageState extends State<ProductListPage>{
               key: Key(model.allProducts[index].title),
               onDismissed: (DismissDirection direction) {
                 if (direction == DismissDirection.endToStart) {
-                  model.selectProduct(model.selectedProductId);
+                  model.selectProduct(model.allProducts[index].id);
                   model.deleteProduct();
                 } else if (direction == DismissDirection.startToEnd) {
-                  print('startToEnd');
+                  print('Swiped start to end');
+                } else {
+                  print('Other swiping');
                 }
               },
-              background: Container(
-                color: Colors.red,
-              ),
+              background: Container(color: Colors.red),
               child: Column(
                 children: <Widget>[
                   ListTile(
                     leading: CircleAvatar(
-
-                      child:Image.network(model.selectedProduct.image),
+                      backgroundImage:
+                      NetworkImage(model.allProducts[index].image),
                     ),
                     title: Text(model.allProducts[index].title),
-                    subtitle: Text('\$${model.allProducts[index].price}'),
-                    trailing: _buildIconButton(context, index, model),
+                    subtitle:
+                    Text('\$${model.allProducts[index].price.toString()}'),
+                    trailing: _buildEditButton(context, index, model),
                   ),
                   Divider()
                 ],
@@ -56,25 +77,6 @@ class _ProductListPageState extends State<ProductListPage>{
           },
           itemCount: model.allProducts.length,
         );
-      },
-    );
-  }
-
-  Widget _buildIconButton(BuildContext context, int index,
-      MainModel model) {
-    return IconButton(
-      icon: Icon(Icons.edit),
-      onPressed: () {
-        model.selectProduct(model.selectedProductId);
-        Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (BuildContext context) {
-              return ProductEditPage();
-            },
-          ),
-        ).then((_){
-          model.selectProduct(null);
-        });
       },
     );
   }
